db2f1f55d5e6753f311761cd394b04d481a9fe1a
rohitkumar91131 <rk34190100@gmail.com>
2025-09-05 03:40:41 +0530

    added user model , auth routesadd . and all done with initialixation

--- config/db.js
+++ config/db.js
@@ -3,16 +3,21 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  String(process.env.DB_PASSWORD) ,
+  String(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
     port: process.env.DB_PORT || 5432,
     logging: false,
+    dialectOptions: process.env.DB_SSL === "true" ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    } : undefined,
   }
 );
 
